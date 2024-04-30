bb85ea327f93360f69749cc753fdd8bb65251d87
Ayush <rasaileeayush17@gmail.com>
2024-04-30 08:17:59 +0545

    DSA

---
+++ LRUCaching.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+
+public class LRUCaching {
+    // int capacity;
+    public static class Node {
+        int k;
+        int v;
+        Node next;
+        Node prev;
+        public Node(int v, int k){
+            this.v = v;
+            this.k = k;
+            this.next = this.prev = null;
+        }
+    }
+    int capacity;
+    HashMap<Integer, Node> map;
+    LRUCaching(int capacity){
+        this.capacity = capacity;
+        map = new HashMap<>();
+    }
+    public void put(int k, int v){
+        if(map.containsKey(k)){
+            //removing the node:
+            remove(map.get(k));
+        }
+        else if(map.size() == capacity){
+            //remove list recently used data
+            remove(tail);
+
+        }
+        else{
+            Node newnode = new Node(v, k);
+            insert(newnode);
+        }
+    }
+    public int get(int k){
+        Node node = map.get(k);
+        if (node==null) {
+            return -1;
+        }
+        remove(node);
+        insert(node);
+        return node.v;
+    }
+
+    Node head = null;
+    Node tail = null;
+    void insert(Node newnode){
+        map.put(newnode.k, newnode);
+        if(head == null || tail == null){
+            head = tail = newnode;
+        }else{
+            newnode.next = head;
+            head.prev = newnode;
+            head = newnode;
+        }
+    }
+
+    void remove(Node node){
+        map.remove(node.k);
+        if(node == head){
+
+        }else if(node == tail){
+
+        }else{
+            
+        }
+    }
+
+}
