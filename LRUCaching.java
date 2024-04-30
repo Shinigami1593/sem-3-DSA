8ede4b51b5686f4133a373ca47207f1f711682a4
Ayush <rasaileeayush17@gmail.com>
2024-04-30 08:28:09 +0545

    DSA

--- LRUCaching.java
+++ LRUCaching.java
@@ -7,36 +7,39 @@ public class LRUCaching {
         int v;
         Node next;
         Node prev;
-        public Node(int v, int k){
+
+        public Node(int v, int k) {
             this.v = v;
             this.k = k;
             this.next = this.prev = null;
         }
     }
+
     int capacity;
     HashMap<Integer, Node> map;
-    LRUCaching(int capacity){
+
+    LRUCaching(int capacity) {
         this.capacity = capacity;
         map = new HashMap<>();
     }
-    public void put(int k, int v){
-        if(map.containsKey(k)){
-            //removing the node:
+
+    public void put(int k, int v) {
+        if (map.containsKey(k)) {
+            // removing the node:
             remove(map.get(k));
-        }
-        else if(map.size() == capacity){
-            //remove list recently used data
+        } else if (map.size() == capacity) {
+            // remove list recently used data
             remove(tail);
 
-        }
-        else{
+        } else {
             Node newnode = new Node(v, k);
             insert(newnode);
         }
     }
-    public int get(int k){
+
+    public int get(int k) {
         Node node = map.get(k);
-        if (node==null) {
+        if (node == null) {
             return -1;
         }
         remove(node);
@@ -46,25 +49,35 @@ public class LRUCaching {
 
     Node head = null;
     Node tail = null;
-    void insert(Node newnode){
+
+    void insert(Node newnode) {
         map.put(newnode.k, newnode);
-        if(head == null || tail == null){
+        if (head == null || tail == null) {
             head = tail = newnode;
-        }else{
+        } else {
             newnode.next = head;
             head.prev = newnode;
             head = newnode;
         }
     }
 
-    void remove(Node node){
+    void remove(Node node) {
         map.remove(node.k);
-        if(node == head){
+        if (node == head) {
+            Node temp = head;
+            head = head.next;
+            head.prev = null;
+            temp.next = null;
 
-        }else if(node == tail){
+        } else if (node == tail) {
+            tail = tail.prev;
+            tail.next = null;
+            node.prev = null;
 
-        }else{
-            
+        } else {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            node.next = node.prev = null;
         }
     }
 
