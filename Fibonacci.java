638c6a9dfe24b2bc76539d5760b5537ae3eaab23
Ayush <rasaileeayush17@gmail.com>
2024-05-12 07:59:13 +0545

    dsa

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,16 +1,26 @@
-public class Fibonacci{
-    int fibo(int n){
+public class Fibonacci {
+    int fibo(int n, int [] dp){
         if(n==0){
             return 0;
         }
         if (n==1) {
             return 1;
         }
-        return fibo(n-1) + fibo(n-2);
+        if(dp[n] != -1){
+
+        }
+        dp[n] =  fibo(n-1,dp) + fibo(n-2,dp);
+        return dp[n];
     }
+
     public static void main(String[] args) {
         Fibonacci f = new Fibonacci();
+        int n = 50;
+        int dp[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
         System.out.println("Fibonacci numbers findings");
-        System.out.println(f.fibo(6));
+        System.out.println(f.fibo(n,dp));
     }
 }
