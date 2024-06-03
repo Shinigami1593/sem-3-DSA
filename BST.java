15c9c60fd0493786fbf0e76e27ac721a802f072f
Ayush <rasaileeayush17@gmail.com>
2024-06-03 08:18:42 +0545

    DSA

--- BST.java
+++ BST.java
@@ -8,6 +8,44 @@ public class BST {
             this.left=this.right=null;
         }
     }
+    Node get(Node root, int data){
+        if(root==null){
+            return root;
+        }
+        if(data<root.data){
+            get(root.left, data);
+        }
+        else if(data>root.data){
+            get(root.right, data);
+        }
+        return root;
+    }
+
+    void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data + " ");
+        inorder(root.right);
+    }
+    void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data + " ");
+    }
+
     Node createBST(Node root, int data){
         if(root==null){
             return new Node(data);
@@ -22,6 +60,40 @@ public class BST {
         return root;
 
     }
+    Node removeNode(Node root, int data){
+        if(root==null){
+            return null;
+        }
+        if(data<root.data){
+            root.left = removeNode(root.left, data);
+        }
+        else if(data>root.data){
+            root.right = removeNode(root.right, data);
+        }
+        else{
+            if(root.left == null){
+                return root.right;
+            }   
+            else if(root.right == null){
+                return root.left;
+            }
+            else{
+                root.data = minValue(root.right);
+                root.right = removeNode(root.right, root.data);
+
+            }
+        }
+        return root;
+    }
+    int minValue(Node root){
+        int min = root.data;
+        while(root.left!= null){
+            min = root.left.data;
+            root=root.left;
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         BST bst = new BST();
         Node root = bst.createBST(null, 11);
--- Trees.java
+++ Trees.java
@@ -8,6 +8,30 @@ public class Trees{
             this.left=this.right=null;
         }
     }
+    void preorder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data + " ");
+        inorder(root.right);
+    }
+    void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data + " ");
+    }
     public static void main(String[] args) {
         Node root = new Node(10);
         root.left = new Node(20);
